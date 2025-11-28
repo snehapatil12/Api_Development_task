c61f82f2060e58c12a9bf16103bcd3101eefde04
Snehal Patil <88209483+snehapatil12@users.noreply.github.com>
2025-11-28 16:01:19 +0530

    Program file

---
+++ using UserManagementAPI.Middleware;  var builder = WebApplication.CreateBuilder(args);  builder.Services.AddControllers(); builder.Services.AddEndpointsApiExplorer(); builder.Services.AddSwaggerGen();  var app = builder.Build();  app.UseSimpleLogger(); /Program.cs
@@ -0,0 +1,17 @@
+using UserManagementAPI.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSimpleLogger(); // custom middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
